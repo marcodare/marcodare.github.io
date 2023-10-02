774aa4259a7ff03c353c1010317e45dc2f4f9569
Marco Da Re <marco.dare@usablenet.com>
2023-10-02 12:40:21 +0200

    coming soon page

--- _next/static/t0g-1AxkMirltkU5CU9fN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-761e7413f855bfa9.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],"/about-me":["static/chunks/pages/about-me-23a899bd465e2891.js"],sortedPages:["/","/_app","/_error","/about-me"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
