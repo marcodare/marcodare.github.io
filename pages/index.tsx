8dea706740037d2ca107f5fab45cc8652183b633
Marco Da Re <marco.dare@usablenet.com>
2023-10-02 12:38:01 +0200

    coming soon page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import Head from 'next/head'
 import Link from 'next/link'
 
 export default function IndexPage() {
   return (
-    <></>
+    <>
+      <Head>
+        <meta http-equiv="Refresh" content="0; url='https://mrcdare.dev/about-me'" />
+      </Head>
+    </>
   )
 }
